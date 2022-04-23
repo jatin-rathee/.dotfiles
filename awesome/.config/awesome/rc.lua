f89fc72c2ce373b2a7a30f90a4f857aabafd96d8
jatin-rathee <jatinrathee2018@gmail.com>
2022-04-23 13:39:17 +0530

    minor fix for nitrogen

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -528,3 +528,7 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 
 -- Gaps
 beautiful.useless_gap = 5
+
+-- Autostart
+awful.spawn.with_shell("picom")
+awful.spawn.with_shell("nitrogen --restore")
